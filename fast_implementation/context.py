48766e21864e2f1a2c86ac20ba0149ad3c08efdf
RyanMoussouni <43774265+RyanMoussouni@users.noreply.github.com>
2020-12-01 18:41:39 +0100

    added comments

--- fast_implementation/context.py
+++ fast_implementation/context.py
@@ -2,19 +2,25 @@
 import pandas as pd
 from functools import reduce
 
-#%% Data
-filePath =  "../ml-latest-small/"
+#%% Aggregation functions
+def agg_as_list(series: pd.Series):
+    return [series.iloc[k] for k in range(len(series))]
+
+#%% Processing Data : merging ratings and movies into userData variable shared accross all files
+# -- change the filePath here
+filePath =  "../ml-latest-small/" 
+
+# -- reading data
 ratings = pd.read_csv(filePath + "ratings.csv")
 movies = pd.read_csv(filePath + "movies.csv")
 links = pd.read_csv(filePath + "links.csv")
 tags = pd.read_csv(filePath + "tags.csv")
+
+# -- merging
 userData = ratings.merge(tags, on= ['userId','movieId'], suffixes= ('_rating','_tag'))
 movieData = movies.merge(links, on= ['movieId'])
 col = ['userId','movieId','rating','timestamp_rating']
 
-
-def agg_as_list(series: pd.Series):
-    return [series.iloc[k] for k in range(len(series))]
-
+# -- aggregation decreases number of rows
 userData = userData.groupby(col, as_index = False).agg(agg_as_list)
 
