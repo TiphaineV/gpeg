ee22c6efab25623cdf8ff3bfa85b6c4a4708796c
RyanMoussouni <43774265+RyanMoussouni@users.noreply.github.com>
2020-12-06 19:38:09 +0100

    added hidden nodes handling

--- fast_implementation/trivialClf.py
+++ fast_implementation/trivialClf.py
@@ -19,26 +19,41 @@ class TrivialClf(_Clf):
         pass
 
     def fit(self, edges):
-        ''' fits the model'''
-
+        '''Fits the recommender system to the input edges
+        '''
         movieNodes = FastGraph.group_by_movie(edges)
         self.sortedMovies = sorted(movieNodes.values(), key= lambda x : x.get_avgRating())
         pass
 
 
-    def predict(self, userIds: np.array, nRec=1):
-        '''Predicts links for the users given.
+    def predict(self, edges, nRec, nHidden= -1)->np.array,list:
+        '''Recommends movies of the users whose Id is contained in the
+        input edges.
+        
         Parameters
         -------
-            userArr: list or dictionnary
+            edges:
+                the edges to predict
 
-        Returns
-        -------
-            recommended movies.
+            nRec:
+                the number of recommendations
+
+            nHidden:
+                shouldn't be changed. It has a use when scoring.
+                
+        Returns:
+        --------
+            predictions array (shape = nUsers, nRec) , userIds in edges
         '''
+        # -- getting users
+        users = FastGraph.group_by_user(edges, nHidden= nHidden)
+
+        # -- prediction for one user (they're all the same)
         userPred = [ movie.nodeId for movie in self.sortedMovies[-nRec:]]
-        pred = [userPred for _ in range(len(userIds))]
-        return np.stack(pred, axis = 0)
+        
+        # -- predictions for all users
+        pred = [userPred for _ in range(len(users))]
+        return np.stack(pred, axis = 0), list(users.keys())
 
 if __name__ =='__main__':
     pass
