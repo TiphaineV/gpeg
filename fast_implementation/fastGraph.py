c0189ee571288b794626be012db5232374853bbe
RyanMoussouni <43774265+RyanMoussouni@users.noreply.github.com>
2020-12-06 19:34:24 +0100

    added handling of hidden edges

--- fast_implementation/fastGraph.py
+++ fast_implementation/fastGraph.py
@@ -11,22 +11,25 @@ Implementation
     Now, why it has been done this way, is because it makes it easy to split the graph (cf
     FastGraph.train_test_split) : you can chose to use only a subset of the edges to tie to the nodes.
 '''
-
 #%% Modules
 #standard
 from abc import ABC, abstractmethod
 import time
+import numpy as np
+import numpy.random as rd
+
 #personal
 from edge import Edge
 from context import userData
 from node import MovieNode, UserNode
-from split import train_test_split
-import numpy.random as rd
+
 
 
 #%% FastGraph class
 class _Graph(ABC):
     def __init__(self):
+        # -- IO
+        print('Graph init ...')
         # -- Attributes
         self.edges = []
         movieIds = set()
@@ -43,30 +46,61 @@ class _Graph(ABC):
         '''
         pass
 
-    @classmethod
-    def group_by_user(cls, edges, degreeMin= 1):
+    def hide_edges(self, indices:list)->list:
+        '''hides edges of the graph whose index is in indices input
         '''
-        can be called by other classes, if not fast change the structure
+        hiddenEdges = []
+        for idx in indices:
+            self.edges[idx].hide()
+            hiddenEdges.append(self.edges[idx])
+        return hiddenEdges
+
+    def unhide_edges(self):
+        '''sets hidden attribute of the edges of the graph to false
+        '''
+        edges = self.edges
+        for edge in edges:
+            edge.hidden = False
+        pass
+
+
+    @classmethod
+    def group_by_user(cls, edges, nLabelsMin=0, degreeMin= 1, nHidden= -1)->dict:
+        '''ties the provided edges to the user Nodes
+        can be called by other classes
         '''
         # -- getting all user nodes
         userNodes = {}
         for idx, edge in enumerate(edges):
             userId = edge.userId
+
+            # -- add the node to the dict if it doesn't exist
             try:
                 userNodes[userId].add(idx)
             except KeyError:
                 node = UserNode(userId)
+                node.add(idx)
                 userNodes.update({userId : node})
 
-        # -- keeping only those with degree > degreeMin
+        # -- keeping only those within a certain range of degree and nLabel
+        #    sometimes (scoring), we also want the nodes with a certain amount of hidden edges. 
         ids = userNodes.keys()
-        return {idd : userNodes[idd] for idd in ids
-                                     if userNodes[idd].get_degree() >= degreeMin}
+        if nHidden == -1:
+            return {idd : userNodes[idd] for idd in ids
+                                             if userNodes[idd].get_degree() >= degreeMin
+                                             if userNodes[idd].get_nLabels() >= nLabelsMin
+                                             }
+        else:
+            return {idd : userNodes[idd] for idd in ids
+                                             if userNodes[idd].get_degree() >= degreeMin
+                                             if userNodes[idd].get_nLabels() >= nLabelsMin
+                                             if userNodes[idd].get_hidden() == nHidden
+                                             }
 
     @classmethod
-    def group_by_movie(cls, edges, degreeMin= 1):
-        '''
-        can be called by other classes, could be faster
+    def group_by_movie(cls, edges, degreeMin= 1)->dict:
+        '''ties the provided edges to the movie Nodes
+        can be called by other classes. Similar to group_by_user
         '''
         movieNodes = {}
         for idx, edge in enumerate(edges):
@@ -75,6 +109,7 @@ class _Graph(ABC):
                 movieNodes[movieId].add(idx)
             except KeyError:
                 node = MovieNode(movieId)
+                node.add(idx)
                 movieNodes.update({movieId : node})
 
         # -- keeping only those with degree > degreeMin
@@ -89,13 +124,20 @@ class _Graph(ABC):
             edges = self.edges
             return [edge for edge in edges if edge.group == group]
 
+
+
+
     def get_movieNodes(self, degreeMin= 1, group= 'all'):
+        '''returns the movieNodes
+        '''
         edges = self.get_edges(group)
         return group_by_movie(edges, degreeMin)
 
-    def get_userNodes(self, degreeMin= 1, group= 'all'):
+    def get_userNodes(self, nLabelMin=0, degreeMin= 1, group= 'all'):
+        '''returns the userNodes
+        '''
         edges = self.get_edges(group)
-        return group_by_user(edges, degreeMin)
+        return group_by_user(edges, nLabelMin, degreeMin)
 
     pass
 
@@ -123,9 +165,12 @@ class FastGraph(_Graph):
             userIds.add(userId)
 
         # -- setting attributes
-        self.edges = edges
+        self.edges = np.array(edges)
         self.movieIds = movieIds
         self.userIds = userIds
+
+        # -- IO
+        print('Done')
         pass
 
     def train_test_split(self, alpha: float):
@@ -148,6 +193,7 @@ class FastGraph(_Graph):
         idxTrain, idxTest = sorted(indexes[nTest:]), sorted(indexes[:nTest])
         nTrain, nTest = len(idxTrain), len(idxTest)
 
+
         # -- Fast implementation : a pointer follows each list.
         k = 0
         ptrTrain = 0
@@ -163,7 +209,6 @@ class FastGraph(_Graph):
                 raise ValueError('2 pointers approach failed')
             k +=1
 
-        self.edges = edges
         return self.get_edges(group= 'train'), self.get_edges(group= 'test')
 
 
@@ -172,6 +217,10 @@ class FastGraph(_Graph):
 
 if __name__ == '__main__':
     pass
+
+
+
+
 
 
 
