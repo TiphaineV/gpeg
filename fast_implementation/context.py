0b1e7a3f90f29e9d2bc2e812502994c4d73c6579
RyanMoussouni <ryan.moussouni@telecom-paristech.fr>
2020-12-25 22:01:49 +0100

    no more assert

--- fast_implementation/context.py
+++ fast_implementation/context.py
@@ -15,7 +15,3 @@ def agg_as_list(series: pd.Series):
 
 # -- reading data
 userData = pd.read_csv('userData.csv')
-
-# -- basic tests
-assert(type(tags['tag'][0]) == list and type(tags['timestamp'][0]) == list) #tester la correspondance si possible aussi
-assert(len(userData) == len(ratings))
